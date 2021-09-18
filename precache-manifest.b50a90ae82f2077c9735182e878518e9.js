000f52d1d30320cf5c0a598e60ffabde2935b794
Nikhil Parekh <nksp777@gmail.com>
2021-09-18 16:56:24 -0700

    Updates

---
+++ precache-manifest.b50a90ae82f2077c9735182e878518e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41fa1b99538b6a6ceb509e7c7406cafd",
+    "url": "/AmazonClone/index.html"
+  },
+  {
+    "revision": "42c1ecfba367cf989e1c",
+    "url": "/AmazonClone/static/css/2.f2378d05.chunk.css"
+  },
+  {
+    "revision": "3d81a9abb691b2e7b4ad",
+    "url": "/AmazonClone/static/css/main.85865760.chunk.css"
+  },
+  {
+    "revision": "42c1ecfba367cf989e1c",
+    "url": "/AmazonClone/static/js/2.a225698f.chunk.js"
+  },
+  {
+    "revision": "d09fde2de45dd0042d9d0a928133c905",
+    "url": "/AmazonClone/static/js/2.a225698f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d81a9abb691b2e7b4ad",
+    "url": "/AmazonClone/static/js/main.8e66a60c.chunk.js"
+  },
+  {
+    "revision": "f1ff0913ce82213eaf23",
+    "url": "/AmazonClone/static/js/runtime-main.cc192980.js"
+  }
+]);
